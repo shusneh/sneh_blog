a6b2d27613706c038736792a51c4612f7e256d34
s_sneh <shubhanshusneh@gmail.com>
2024-07-09 15:29:55 +0530

    Routing has done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home  from './pages/Home'
+import About from './pages/About'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+// import About from './pages/About'
 
 export default function App() {
   return (
-    <div className='text-3xl text-red-300 text-center' >
-      app
-    </div>
+   <BrowserRouter>
+    <Routes>
+      <Route  path='/' element={<Home/>}/> 
+      <Route path='/about' element={<About/>} />
+      <Route path='/dashboard' element={<Dashboard/>} />
+      <Route path='/projects' element={<Projects/>} />
+      <Route path='/signin' element={<SignIn/>} />
+      <Route path='/signup' element={<SignUp/>} />
+      
+    </Routes>
+
+   </BrowserRouter>
   )
 }
