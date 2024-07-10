1e62df3486ee351ee1d2f6e4bf9395ce6a7d0d0a
s_sneh <shubhanshusneh@gmail.com>
2024-07-10 21:16:53 +0530

    user routes and controller created

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import userRoutes from './routes/user.route.js'
 
 dotenv.config();
 mongoose.connect(process.env.MONGO)
@@ -8,6 +9,8 @@ mongoose.connect(process.env.MONGO)
 .catch((err)=>console.log(err));
 
 const app = express();
+
+app.use('/api/user',userRoutes)
 
 app.listen(3000,()=>{
     console.log("server is running at 3000!");
