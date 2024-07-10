01591d40a9de2f80a29586a1e8f7cea0818ec039
s_sneh <shubhanshusneh@gmail.com>
2024-07-10 23:47:08 +0530

    created signup funtionalities

--- api/index.js
+++ api/index.js
@@ -2,15 +2,18 @@ import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import userRoutes from './routes/user.route.js'
+import authRoutes from './routes/auth.route.js' 
 
 dotenv.config();
 mongoose.connect(process.env.MONGO)
-.then(console.log('Database is connected'))
+.then(console.log('Database is connected')) 
 .catch((err)=>console.log(err));
 
 const app = express();
+app.use(express.json());
 
-app.use('/api/user',userRoutes)
+app.use('/api/user',userRoutes);
+app.use('/api/auth',authRoutes)
 
 app.listen(3000,()=>{
     console.log("server is running at 3000!");
