88f3a5a277e08b0a64e48d3253a27736f9eb7c05
s_sneh <shubhanshusneh@gmail.com>
2024-07-12 13:06:11 +0530

    created an errorHandler middleware

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,25 +1,26 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs'
+import { errorHandler } from "../utils/error.js";
 
 export const signup = async(req,res)=>{
     const {username, email, password} = req.body;
 
     if(!username||!email|| !password || username==="" || email==="" || password===""){
-        return res.status(400).json({message : "All fields are required"});
+        next(errorHandler(400, "All fields are mandatory"));
     }
 
     const hashPassword = bcryptjs.hashSync(password,10);
     const newUser = new User({
         username,
         email,
-        password : hashPassword
-    });
+        password : hashPassword 
+    }); 
 
     try {
         await newUser.save();
         res.status(200).json({message : "SignUp Successful"});    
     } catch (err) {
-        res.json({message : err.message});
+        res.json({message : err.message}); // why we cant ue next here !!
     }
 
     
--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv'
 import userRoutes from './routes/user.route.js'
 import authRoutes from './routes/auth.route.js' 
 
+
 dotenv.config();
 mongoose.connect(process.env.MONGO)
 .then(console.log('Database is connected')) 
@@ -14,6 +15,7 @@ app.use(express.json());
 
 app.use('/api/user',userRoutes);
 app.use('/api/auth',authRoutes)
+
 
 app.listen(3000,()=>{
     console.log("server is running at 3000!");
