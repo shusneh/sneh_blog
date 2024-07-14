4c7c5c8e4ea82fe547bec385b3a94b2640d503e2
s_sneh <shubhanshusneh@gmail.com>
2024-07-14 15:26:59 +0530

    create signin api route

--- api/utils/error.js
+++ api/utils/error.js
@@ -1,7 +1,7 @@
 export const errorHandler = (statusCode, message)=>{
     const error = new Error();
 
-    error.statusCode = statusCode
+    error.statusCode = statusCode;
     error.message = message;
     return error;
 }
