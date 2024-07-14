7b6adff61e520b41ae67aabc02a3f69a8cbe51ac
s_sneh <shubhanshusneh@gmail.com>
2024-07-14 10:24:22 +0530

    footer section completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import Projects from './pages/Projects'
 import SignIn from './pages/SignIn' 
 import SignUp from './pages/SignUp'
 import Header from './components/Header'
+import Footer from './components/Footer'
 // import About from './pages/About' 
 
 export default function App() {
@@ -21,6 +22,7 @@ export default function App() {
       <Route path='/sign-up' element={<SignUp/>} />
       
     </Routes>
+   <Footer/>
 
    </BrowserRouter>
   )
