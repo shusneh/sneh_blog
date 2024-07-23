6fba5e0815c9f26befa96b6dec6346393059e500
s_sneh <shubhanshusneh@gmail.com>
2024-07-23 17:50:09 +0530

    upadate user api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,6 +1,8 @@
 import express from 'express'
 const router = express.Router();
-import {test} from '../controllers/user.controller.js'
+import {test, updateUser} from '../controllers/user.controller.js'
+import {verifyToken} from '../utils/verify.js';
 router.get('/test',test);
+router.put('/update/:userId', verifyToken, updateUser);
 
 export default router;
